0b1315fdb6cd2a76605b04fefe13fcbb79c593a8
Phil Bauma <evlukoobauma@gmail.com>
2024-08-24 22:07:44 +0200

    Data base added

--- phpMyAdmin.php
+++ phpMyAdmin.php
@@ -53,58 +53,72 @@
     // echo "Creation successful";
 //================================================================
 
-$client = "CREATE TABLE Clients(
-    ClientID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    Nom VARCHAR(50),
-    Postnom VARCHAR(50),
-    Adresse VARCHAR(255),
-    Email VARCHAR(30),
-    NumeroPhone VARCHAR(15))";
+$client = "CREATE TABLE Clients (
+    ClientID INT PRIMARY KEY,
+    Nom VARCHAR(200),
+    Prenom VARCHAR(200),
+    Adresse VARCHAR(200),
+    Email VARCHAR(200),
+    NumeroTelephone VARCHAR(30)
+)";
 $connexion->exec($client);
-echo "Creation Client table reussi"."<br>";
 
-$employer = "CREATE TABLE Employers(
-EmployerID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-Nom VARCHAR(50),
-Postnom VARCHAR(50),
-Adresse VARCHAR(255),
-Email VARCHAR(30),
-NumeroPhone VARCHAR(15))";
-$connexion->exec($employer);
-echo "Creation Employer table reussi"."<br>";
-
-
-$fournisseur = "CREATE TABLE Fournisseurs(
-FournisseurID INT AUTO_INCREMENT PRIMARY KEY,
-NomFournisseur VARCHAR(50),
-Postnom VARCHAR(50),
-Adresse VARCHAR(255),
-Email VARCHAR(30),
-NumeroPhone VARCHAR(50))";
-$connexion->exec($fournisseur);
-echo "Creation Fournisseur table reussi"."<br>";
-
-$produit = "CREATE TABLE Produits(
-ProduitID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-Nom VARCHAR(70),
-Details VARCHAR(255),
-Prix DECIMAL(10,2),
-FournisseurID INT,
-FOREIGN KEY (fournisseurID) REFERENCES Fournisseurs(fournisseurID)
+// Création de la table Fournisseurs
+$fournisseur = "CREATE TABLE Fournisseurs (
+	FournisseurID INT PRIMARY KEY,
+    NomFournisseur VARCHAR(200),
+    Adresse VARCHAR(100),
+    Email VARCHAR(200),
+    NumeroTelephone VARCHAR(200)
+    )";
+    $connexion->exec($fournisseur);
+    
+// Creation de la table employe
+$employer = "CREATE TABLE Employes (
+	EmployeID INT PRIMARY KEY,
+    Nom VARCHAR(200),
+    Prenom VARCHAR(200),
+    Fonction VARCHAR(200),
+    Email VARCHAR(200),
+    NuméroTelephone VARCHAR(200)
+    )" ;
+    $connexion->exec($employer);
+ 
+ // Insertion des données dans les table fournisseurs
+ // INSERT INTO Fournisseurs (FournisseurID, NomFournisseur, Adresse, Email, NumeroTelephone)
+ // VALUES (1233, "Steve", "Rue Voltaire", "steve2024@gmail.com", "07 54 34, 72"),
+// (1444, "Arthur", "Rue Lumière", "arthur024@gmail.com", "07 54 34, 72");
+ 
+ // Modification de la table Produits pour intégrer l'identifiant du fournisseur
+ // Création de la table "Produits"
+$produit = "CREATE TABLE Produits (
+    ProduitID INT PRIMARY KEY,
+    NomProduit VARCHAR(200),
+    Description TEXT,
+    PrixUnitaire DECIMAL(10, 2),
+   FournisseurID INT,
+   FOREIGN KEY(FournisseurID)  REFERENCES Fournisseurs(FournisseurID)
 )";
 $connexion->exec($produit);
-echo "Creation Produit table reussi";
 
-
-$vente = "CREATE TABLE Ventes(
-VenteID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-Nom VARCHAR(50),
-Postnom VARCHAR(50),
-Adresse VARCHAR(255),
-Email VARCHAR(30),
-NumeroPhone VARCHAR(50))";
-$connexion->exec($client);
+ // Modification de la création de la table vente pour intégrer l'identifiant de l'employe
+$vente = "CREATE TABLE Ventes (
+    VenteID INT PRIMARY KEY,
+    DateVente DATE,
+    ClientID INT,
+    ProduitID INT,
+    EmployeID INT,
+    FOREIGN KEY(ClientID) REFERENCES Clients(ClientID),
+    FOREIGN KEY(ProduitID)  REFERENCES Produits(ProduitID),
+    FOREIGN KEY(EmployeID) REFERENCES Employes(EmployeID),
+    QuantiteVendue INT,
+    MontantTotal DECIMAL(10, 2)
+)";
+$connexion->exec($vente);
 echo "Creation Vente table reussi";
+
+/*
+
 /*$connexion->exec($client);
 $connexion->exec($fournisseur);
 $connexion->exec($employer);
